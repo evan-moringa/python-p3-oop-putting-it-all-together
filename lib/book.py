fff23f3690ef2335d60c00bdde010de23c1cbbad
Evan Keya <evan.keya@student.moringaschool.com>
2023-11-30 02:44:44 +0300

    done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, author, page_count):
+        if not isinstance(page_count, int):
+            raise TypeError("page_count must be an integer")
+
+        self.title = title
+        self.author = author
+        self.page_count = page_count
+        self.current_page = 1
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+        self.current_page += 1
+
+
+
+
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
+
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        self.brand = brand
+        self.size = size
+        self.condition = "New"
+
+    def repair(self):
+        print("The shoe has been repaired.")
+        self.condition = "New"
+    
